c69eb00b3eb945a3daaa8fe93668d6359dfd9baa
FilipLe <48429871+FilipLe@users.noreply.github.com>
2025-05-24 10:36:03 -0400

    [feat] up to chapter 2

---
+++ src/components/data/bookEN.jsx
@@ -0,0 +1,252 @@
+const pages = [
+    {   page:"1",
+        header:true,
+        headerContent:"Table of Content",
+        content:"Note to Reader…………………………………………2\n\n\
+Chapter 1: Early Days…………………………………4\n\n\
+Chapter 2: From Thanh Hoá to Kraków……………10\n\n\
+Chapter 3: Undergrad in Kraków……………………14\n\n\
+Chapter 4: Return to Vietnam………………….……11\n\n\
+Chapter 5: PhD, Motherhood, and Legacy…..……13\n\n\
+Epilogue…………………………………………….…16\
+"
+    },
+    
+    {   page:"2",
+        header:true,
+        headerContent:"Note to Readers",
+        content:(
+            <>
+                <img 
+                    src="https://i.postimg.cc/mZjsqx0Y/me-and-grandma.jpg" 
+                    alt="My Grandma and I" 
+                    style={{ 
+                        display: 'block', 
+                        margin: '0 auto', 
+                        width: '70%', 
+                        maxHeight: '300px', 
+                        objectFit: 'cover' 
+                    }} 
+                />
+                <p className = "indent">I am Nguyễn Thị Thoa’s grandson. I have written this as a birthday gift for my amazing grandmother—and as a legacy for our family. If it were not for her, our lives would not be what they are today. I wanted to write this book to ensure that her story, her sacrifices, and her strength are never forgotten. It is not every day that a woman walks through fire and walks out carrying her children—and her future—on her back. It would be a waste to let her story go untold.</p>
+                <p className = "indent">This book is written from stories passed down, memories shared, and truths felt. Some details may </p>
+            </>
+        )
+    },
+
+    {   page:"3",
+        header:false,
+        headerContent:"",
+        content:(
+            <>
+                <p>have blurred with time, but the heart of it—her strength—remains sharp and clear.</p>
+                <p className = "indent">To my babcia, my mama, and the rest of my family: Thank you. Thank you for the strength, the wisdom, the love, and the sacrifices that built the life I am privileged to live today. This book is for you. This legacy is yours.</p>
+                <br/><br/>
+                <img
+                    src="https://i.postimg.cc/28L0WhF1/AB51844-F-07-E7-4-E1-D-85-ED-100-D5-C11-F356-1-105-c.jpg"
+                    alt="The Whole Family" 
+                    style={{ 
+                        display: 'block', 
+                        margin: '0 auto', 
+                        width: '70%', 
+                        maxHeight: '300px', 
+                        objectFit: 'cover' 
+                    }} 
+                />
+                <p className = "photo-caption">The Whole Family (London, 2024)</p>
+            </>
+        )
+    },
+
+    {   page:"4",
+        header:true,
+        headerContent:"Chapter 1: Early Days",
+        content:(
+            <>
+                <p className = "indent">On 1/6/1949, in the rural province of Thanh Hoá, Vietnam was born my grandmother, Nguyễn Thị Thoa—or at least, that was the date many years later recorded on her birth certificate, hastily created when she needed one to begin elementary school. In truth, her mother, like many others in the village at the time, was illiterate and could not read a calendar—only recalling that her daughter was born in the year of the Buffalo. Life was not romantic—it was survival dressed in the thinnest linen. She was the youngest of the nine children, born into a childhood shaped by war, scarcity, and the enduring strength of a close-knit community.</p>
+                <p className = "indent">The Vietnam War broke out in the mid-1950s, a brutal conflict between the communist North and the US-backed anti-communist South. Thanh Hoá, located just in the center of Vietnam, became a strategically important region — the one connecting the two regions and ultimately enabling a supply route and a military stronghold for the Viet Congs fighting in South Vietnam. Hence, due to having key infrastructure like the Hàm</p>
+            </>
+        )
+    },
+
+    {   page:"5",
+        header:false,
+        headerContent:"",
+        content:(
+            <>
+                <p>Rồng Bridge, the province endured repeated bombings by American B-52 bomber planes relentlessly.</p>
+                <img
+                    src="https://i.postimg.cc/L6VnN0Vw/ham-rong-bridge.jpg"
+                    alt="Ham Rong Bridge" 
+                    style={{ 
+                        marginTop: '1.5em',
+                        display: 'block', 
+                        margin: '0 auto', 
+                        width: '70%', 
+                        maxHeight: '300px', 
+                        objectFit: 'cover' 
+                    }} 
+                />
+                <p className = "photo-caption">Ham Rong Bridge (Thanh Hoá, Vietnam War)</p>
+                <p className = "indent">In those years, the war crept into every corner of my grandmother’s life — including her fight to learn. Her path to education was never guaranteed. At the time, Vietnam faced a serious illiteracy crisis, and worse yet, my grandmother carried a political stain: she was considered “con cháu địa chủ”—descendant of a landowning family.</p>
+                <p className = "indent">That label marked her for discrimination under the new communist state. Schools turned away children like her. But her older sister, who had finished tenth grade and could read, began teaching my grandmother at</p>
+            </>
+        )
+    },
+
+    {   page:"6",
+        header:false,
+        headerContent:"",
+        content:(
+            <>
+                <p>home, using whatever scraps of material that she had remaining. Then one day, a brave local official—whose name my grandmother still remembers with reverence—challenged the policy and declared that every child deserves to learn, that she was finally allowed to attend school.</p>
+                <p className = "indent">But even then, my grandmother’s education did not unfold in a classroom. It took place in a bunker beneath the ground that she and her classmates had dug with their own hands. Together, they huddled underground, whispering conjugations as B-52 bombers rumbled overhead. No matter the hour or lesson, one student was always stationed at the entrance—on guard, eyes sharp, a stick or rusted blade in hand—ready to alert the others at the first sign of danger. They rotated in silence, a shifting roster of children learning and watching, watching and learning, their childhood measured not by bells, but by bombs.</p>
+                <p className = "indent">Above ground, life was no less exacting. During the</p>
+            </>
+        )
+
+    },
+
+    {   page:"7",
+        header:false,
+        headerContent:"",
+        content:(
+            <>
+                <p>communist period, poverty was not just widespread—it was systemized. With no free market, there was no way to earn more, save more, or build a better life through individual effort. My grandmother was the youngest of nine, born into a family that survived on resourcefulness and grit. Her parents, though unable to read or write, were masterful in the art of making. Her father built the family’s beds, roofs, and farming tools with bare hands and scrap wood, metal, and whatever the land offered. Her mother farmed rice, fermented fish sauce, and dried what little they caught to stretch each meal across the week. What you ate or used, you had to produce yourself.</p>
+                <p className = "indent">However even then, under the communist state, not all of it was yours to keep. What little they grew, caught, or made, they were obligated to surrender to share with the state. Every grain of rice was fought for. The family’s most valuable asset was not jewelry or land, but a single buffalo for its ability to pull the plow and bring life from the soil. One day, word spread that officials were coming</p>
+            </>
+        )
+        
+    },
+
+    {   page:"8",
+        header:false,
+        headerContent:"",
+        content:(
+            <>
+                <p>to confiscate it. My grandmother, barely a teenager, released the animal into the wild rather than watch it be beaten and forced into labor by strangers. Her family cried. It was like losing a member of the family.</p>
+                <img   
+                    src="https://i.postimg.cc/PJTdDT0x/grandma-and-siblings.jpg"
+                    alt="Grandma and Siblings" 
+                    style={{ 
+                        marginTop: '1.5em',
+                        display: 'block', 
+                        margin: '0 auto', 
+                        width: '70%', 
+                        maxHeight: '300px', 
+                        objectFit: 'cover' 
+                    }} 
+                />
+                <p className = "photo-caption">My grandmother (top left) and her siblings in late 90s</p>
+                <p className = "indent">Even so, life pressed on. There was no time to mourn for long. Survival allowed no pauses. Day by day, week by week, year by year, they endured—tilling the fields by hand, rationing what little was left after the state’s share, and pushing through each season with</p>
+            </>
+        )
+
+    },
+
+    {   page:"9",
+        header:false,
+        headerContent:"",
+        content:(
+            <>
+                <p>quiet resilience. During the day, my grandmother attended school; in the evenings, she returned home to help her family.</p>
+                <p className = "indent">One afternoon in 1967, everything changed. My grandmother had just returned from school when her family received a letter. She had been summoned by the state to the capital, Hanoi — she had been selected as the top student in her town for “du học” (to study abroad). She didn’t even know what the word meant. All she knew was that she had to go...</p>
+                <img
+                    src="https://i.postimg.cc/MHmK3v1V/1967.jpg"
+                    alt="Grandma in 1967"
+                    style = {{
+                        marginTop: '1.2em',
+                        display: 'block', 
+                        margin: '0 auto', 
+                        width: '30%', 
+                        maxHeight: '300px', 
+                        objectFit: 'cover' 
+                    }}
+                />
+                <p className = "photo-caption">My grandmother in 1967</p>
+                <p>...a new chapter was beginning, though she didn’t yet know what it would hold.</p>
+            </>
+        )
+    },
+
+    {   page:"10",
+        header:true,
+        headerContent:"Chapter 2: From Thanh Hoá to Kraków",
+        content:(
+            <>
+                <p className = "indent">Getting to Hanoi was another matter. The country was still in the throes of war. To reach Hanoi from Thanh Hóa, she had to travel at night, hidden in pig-transportation trucks to avoid detection from American bombers. The menacing B52s would strike anything that moved by day. Her mother had packed sticky rice balls (nắm cơm) for the journey, tucking them carefully into a cloth bundle. But my grandmother was small and slight, and as she strained to hoist herself into the back of the truck—gripping the wooden frame, her legs barely reaching the edge—the bundle slipped from her hands and fell to the ground. She had no time to retrieve it. The truck was already pulling away. So she rode hungry, her stomach empty, her hands clinging to the crate as the wheels carried her deeper into the night.</p>
+                <p className = "indent">The journey was long, anxious, and uncertain. They slept during the day in strangers’ homes, hiding among villagers, sometimes without food. Each morning, they would wake to the sound of distant gunfire and the scent of wet soil. At night, the driver would whisper, "Get in,"</p>
+            </>
+        )
+    },
+
+    {   page:"11",
+        header:false,
+        headerContent:"",
+        content:(
+            <>
+                <p>and the engine would sputter like an old man trying to breathe. The truck bumped along broken roads, its wheels grinding over gravel and cratered earth left behind by bombs. Nights passed in silence. Time blurred. Each village looked like the last—dark, quiet, half-empty. They switched trucks more than once, ducking under tarps, sometimes sharing space with livestock. And then, finally, the skyline changed. She had arrived — Hanoi.</p>
+                <p className = "indent">Once in Hanoi, she learned that she had been awarded a scholarship to study Chemistry in Kraków, Poland. The government assigned the smartest students across the nation to fields they deemed necessary for the country’s progress. Chemistry, it would be. She didn’t question it. She had grown used to obeying orders larger than herself. At 18, she weighed just 36 kilograms. A small girl with a large dream she never asked for.</p>
+                <p className = "indent">From there, she boarded a train that would take her</p>
+            </>
+        )
+    },
+
+    {   page:"12",
+        header:false,
+        headerContent:"",
+        content:(
+            <>
+                <p>across continents. A ten-day journey. The days blurred past windows smudged with dust and snow, the landscape shifting from mountains to flat plains to endless forests. The air inside the compartments was stale, heavy with the scent of wool and metal. Meals were handed out occasionally, but nothing looked or tasted familiar. Once, they were served bread and cottage cheese—soft, white, strange. One boy shouted, “Tụi nó cho mình ăn vôi kìa!” —“They’re feeding us construction hydrated lime!” The entire group fell silent.</p>
+                <img
+                    src="https://i.postimg.cc/bvwbxjcm/journey-grandma-w-college-friends.jpg"
+                    alt="Grandma and her classmates"
+                    style={{
+                        marginTop:'1.3em',
+                        display:'block',
+                        margin:'0 auto',
+                        width:'70%',
+                        maxHeight:'300px',
+                        objectFit:'cover'
+                    }}
+                />
+                <p className = "photo-caption">Grandmother and her colleagues to Poland</p>
+            </>
+        )
+    },
+
+    {   page:"13",
+        header:false,
+        headerContent:"",
+        content:(
+            <>
+                <p>For the rest of the journey, none of the Vietnamese students touched their food again. Ten days of hunger, of foreign signs and foreign fears. First winding through China, then passing through Russia, and finally to Kraków, Poland.</p>
+                <img
+                    src="https://i.postimg.cc/B6tnYYtc/year-unknown-grandma.jpg"
+                    alt="Grandmother, 18"
+                    style={{
+                        marginTop:'1.6em',
+                        display:'block',
+                        margin:'0 auto',
+                        width:'50%',
+                        maxHeight:'300px',
+                        objectFit:'cover'
+                    }}
+                />
+                <p className = "photo-caption">My Grandmother, 1967</p>
+            </>
+        )
+    },
+
+    {   page:"14",
+        header:true,
+        headerContent:"Chapter 3: Undergrad in Kraków",
+        content:(
+            <>
+                <p className = "indent"></p>
+            </>
+        )
+    }
+]
+
+export default pages;
